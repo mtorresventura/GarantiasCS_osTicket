8cdee607a88ee9bf208632dee8e8e59884056bb4
marco torres <mtorresventura@gmail.com>
2019-09-29 15:51:02 -0500

    primer commit de tesis: gestión de mesa de ayuda - compusoft

---
+++ upload/setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="http://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.4.0.min.js?dca01e1"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?dca01e1"></script>
+    <script type="text/javascript" src="js/setup.js?dca01e1"></script>
+    <script type="text/javascript" src="js/tips.js?dca01e1"></script>
+</body>
+</html>
